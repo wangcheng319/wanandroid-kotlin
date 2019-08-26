145d8f0e4196daa14f46e1b07be424bfc6ef076d
王成 <274281610@qq.com>
2019-08-26 17:34:57 +0800

    recyclerview adapter databinding

--- app/src/main/java/com/example/wanandroid_kotlin/adapter/TestAdapter.kt
+++ app/src/main/java/com/example/wanandroid_kotlin/adapter/TestAdapter.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wanandroid_kotlin.R
+import com.example.wanandroid_kotlin.databinding.ItemHomeBinding
 import com.example.wanandroid_kotlin.model.Article
 
 class TestAdapter(private val mContext: Context, private val datas: List<Article.Datas>) :
@@ -19,16 +20,24 @@ class TestAdapter(private val mContext: Context, private val datas: List<Article
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.mTv.text = datas[position].chapterName
+//        holder.mTv.text = datas[position].chapterName
+        datas[position].chapterName?.let { ViewHolder.bind(holder, it) }
     }
 
     override fun getItemCount(): Int {
-        return datas?.let { it.size }
+        return datas.size
     }
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-         val mTv: TextView = itemView.findViewById(R.id.textView)
+//         val mTv: TextView = itemView.findViewById(R.id.textView)
+        private var dataBinding: ItemHomeBinding? = DataBindingUtil.bind(itemView)
+
+        companion object {
+            fun bind(viewHolder: ViewHolder, string: String){
+                viewHolder.dataBinding?.item = string
+            }
+        }
 
     }
 }
